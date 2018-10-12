1ecaff0f6c9202e2a1cb4d5b26c3f7d9deff4f9d
Jordan Power <Jordan.Power@edifecs.com>
2018-10-12 16:17:13 -0700

    Initial commit for project.

---
+++ src/extensions/topMenu/TopMenuApplicationCustomizer.ts
@@ -0,0 +1,81 @@
+import { override } from "@microsoft/decorators";
+import { Log } from "@microsoft/sp-core-library";
+import {
+    BaseApplicationCustomizer,
+    PlaceholderContent,
+    PlaceholderName
+} from "@microsoft/sp-application-base";
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import * as strings from "TopMenuApplicationCustomizerStrings";
+
+const LOG_SOURCE: string = "TopMenuApplicationCustomizer";
+
+/**
+ * If your command set uses the ClientSideComponentProperties JSON input,
+ * it will be deserialized into the BaseExtension.properties object.
+ * You can define an interface to describe it.
+ */
+export interface ITopMenuApplicationCustomizerProperties {
+    // This is an example; replace with your own property
+    TermSetId: string;
+}
+
+/** A Custom Action which can be run during execution of a Client Side Application */
+export default class TopMenuApplicationCustomizer extends BaseApplicationCustomizer<
+    ITopMenuApplicationCustomizerProperties
+> {
+    private _topPlaceholder: PlaceholderContent | undefined;
+
+    @override
+    public onInit(): Promise<void> {
+        Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
+
+        // Added to handle possible changes on the existence of placeholders.
+        this.context.placeholderProvider.changedEvent.add(
+            this,
+            this._renderPlaceHolders
+        );
+
+        // Call render method for generating the HTML elements.
+        this._renderPlaceHolders();
+
+        return Promise.resolve();
+    }
+
+    private _renderPlaceHolders(): void {
+        this.context.placeholderProvider.placeholderNames
+            .map(name => PlaceholderName[name])
+            .join(", ");
+
+        if (!this._topPlaceholder) {
+            this._topPlaceholder = this.context.placeholderProvider.tryCreateContent(
+                PlaceholderName.Top
+            );
+
+            // The extension should not assume that the expected placeholder is available.
+            if (!this._topPlaceholder) {
+                console.error("The expected placeholder (Top) was not found.");
+                return;
+            }
+
+            if (this.properties) {
+                let termSetId: string = this.properties.TermSetId;
+                if (!termSetId) {
+                    termSetId = "(Top property was not defined.)";
+                }
+
+                if (this._topPlaceholder.domElement) {
+                    this._topPlaceholder.domElement.innerHTML = `
+                        <div class="">
+                            <div class="">
+                                <i class="ms-Icon ms-Icon--Info" aria-hidden="true"></i> ${escape(
+                                    termSetId
+                                )}
+                            </div>
+                        </div>`;
+                }
+            }
+        }
+    }
+}
